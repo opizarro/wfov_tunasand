a9d04c8df3a599d5eac3243280fa47ac6d6b2991
opizarro <opizarro73@gmail.com>
2016-07-18 10:05:16 +0900

    Add files via upload
    initial scripts

---
+++ add_images_to_RAW.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+# add_images__to_RAW iDIR RAWoutdir
+# takes a directory with images
+# and assembles a log file with VIS: entries consistent with seabed_localisation
+
+
+import os
+import glob
+import re
+import time
+import calendar
+import sys
+
+#rawfile = '/media/lagoon/RAW_DATA/ScottReef200907/r20090726_074343_scott_05_long_transect_auv2/d20090726_074343/20090726_0743.RAW.auv'
+#idir = '/Users/opizarro/research/data/seagras
+#idir = '/Users/opizarro/research/data/ScottReef200907/r20090726_074343_scott_05_long_transect_auv2/i20090726_074343_cv/*.png'
+#idir = '/media/lagoon/RAW_DATA/ScottReef200907/r20090726_074343_scott_05_long_transect_auv2/i20090726_074343/*.pgm'
+# example of image name PR_20081012_021022_110_LC16.jpg
+
+# read in arguments
+idir = sys.argv[1]
+rawoutdir = sys.argv[2]
+
+
+# list of images
+imlist = glob.glob(idir + '/*.tif')
+imlist.sort()
+nimages = len(imlist)
+
+#print "\nDirectories:\nLOGS: " + rawdir + "\nIMGS: " + idir
+
+if nimages==0 :
+    print("\n\nNo IMAGES found! Nothing to merge. Exiting\n")
+    quit()
+else:
+    print("\n\nCreating log entries for "+str(nimages)+ " \n")
+
+# create the new log file that will merge both sources of info
+(logpath,logname) = os.path.split(idir)
+
+if not os.path.isdir(rawoutdir):
+    os.makedirs(rawoutdir)
+
+flog_merge = open(rawoutdir+"/VIS"+logname,'w')
+
+for imfullpath in imlist:
+    (impath,im) = os.path.split(imfullpath)
+    #print imfullpath
+    #print im
+    # determine time from file name
+    tyear = int(im[3:7])
+    tmonth = int(im[7:9])
+    tday = int(im[9:11])
+    thour = int(im[12:14])
+    tminute = int(im[14:16])
+    tsecond = float(im[16:18] + '.' + im[19:22])
+        
+    # print str(tyear) + ' ' + str(tmonth) + ' ' + str(tday) + ' ' + str(thour) + ' ' + str(tminute) + ' ' + str(tsecond)
+        
+    #easy_tstring = im[3:15] + tsecond
+        
+    #tstruc = time.strptime(
+        
+    tunix = calendar.timegm([tyear,tmonth,tday,thour,tminute,tsecond])
+    tstruc = time.gmtime(tunix)
+
+    # swap extension
+    logentry = "VIS: " + "%.3f" % tunix + " [" + "%.3f" % tunix + "] " + im[:-3] + "tif" + "\n"
+        #logentry = "VIS: " + "%.3f" % tunix + " [" + "%.3f" % tunix + "] " + im[:-3] + "png" + "\n"
+
+    flog_merge.write(logentry)
+
+flog_merge.close()
+
+        
+                
+
+        
---
+++ parse_GAPS.py
@@ -0,0 +1,88 @@
+# read a GAPS standard output file
+# particular string of interest is  $PTSAG
+# Transponder Absolute Positioning Message (geographical coordinates and depth)
+# $PTSAG,#NNNNN, hhmmss.sss,jj,mm,aaaa,BBB,DDMM.MMMMM,H,DDDMM.MMMMM,D,A,MMMM.M,A, MMMM.M *CK
+# want to write to something like
+#fprintf(fid,'GPS_RMC: %.3f\t Lat:%.6f N Lon:%.6f W Bad:0 A Spd:0 Crs:0 Mg:nan\n',sps.t(k),sps.lat(k),sps.lon(k));
+
+import pynmea2
+import string
+import datetime
+
+
+
+
+#GAPSlog = open('/Users/opizarro/data/TunaSand/GAPSexample/20140605163#709-001.dat')
+GAPSlog = open('/Users/opizarro/data/TunaSand/Dive_15/GAPS/20160714124941-003.dat')
+
+GAPS_rawlog = open('/Users/opizarro/Downloads/test.RAW.auv','w')
+
+
+#head=GAPSlog.readline()
+#print head
+
+def dm2dd(dm ):
+    degrees = int(dm/100)
+    minutes = dm-degrees*100
+    dd = float(degrees) + float(minutes)/60 ;
+    return dd;
+
+
+streamreader = pynmea2.NMEAStreamReader()
+for line in GAPSlog:
+    try:
+        #strip leading <
+        cleanline = line.strip('< ')
+        msg = pynmea2.parse(cleanline)
+
+        #print msg
+        #print msg.data
+        #print msg.identifier()
+        #print vars(msg)
+        #print see(msg)
+        #print 'parsed from ' + line
+    except:
+        print('could not parse ' + line )
+        continue
+
+    if msg.identifier()=='PTSA' and msg.data[0]=='G':
+        print('message PTSAG')
+        #$PTSAG,#02754,043516.771,14,07,2016,0,2640.20937,N,12751.97519,E,F,0000.0,0,0000.0*2A
+             #0,    1 ,  2       ,3 ,4 , 5  ,6, 7        ,8, 9         ,10,
+        # get timestamp lat and lon and print out RAW message
+
+        if msg.data[6]=='1':   # transponder number for AUV
+            print(msg)
+            LatDM = msg.data[7]
+            Lat = dm2dd(float(LatDM))
+            NS = msg.data[8]
+
+            LonDM = msg.data[9]
+            Lon = dm2dd(float(LonDM))
+            EW = msg.data[10]
+
+            HMS = msg.data[2]
+            hh = HMS[0:2]
+            mm = HMS[2:4]
+            ss = HMS[4:6]
+            micros = HMS[7:10]
+
+            day = msg.data[3]
+            month = msg.data[4]
+            year = msg.data[5]
+
+
+
+            timestring = year + ' ' + month + ' ' + day + ' ' + hh +':' + mm + ':' + ss
+            print(timestring)
+            dt= datetime.datetime.strptime(timestring, "%Y %m %d %H:%M:%S")
+
+
+            utime_nomicro = (dt - datetime.datetime(1970,1,1))/datetime.timedelta(seconds=1)
+
+            #withmicroseconds
+            utime = utime_nomicro + float(micros) / 1000
+
+            outputstr = 'GPS_RMC: {:.3f}\t Lat:{:.6f} {} Lon:{:.6f} {} Bad:0 A Spd:0 Crs:0 Mg:nan\n'.format(utime,Lat,NS,Lon,EW)
+            print(outputstr)
+            GAPS_rawlog.write(outputstr)
